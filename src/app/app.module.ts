909447bdb94aec227d96750ee14d85343c23c993
Peerapat Jommanee <QuEz@Peerapats-MacBook-Pro.local>
2017-07-23 23:01:10 +0700

    add dndMudule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
 import 'hammerjs';
+import { DndModule } from 'ng2-dnd';
 
 import { AppComponent } from './app.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
@@ -19,7 +20,8 @@ import { ManageMenuComponent } from './manage-menu/manage-menu.component';
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DndModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
